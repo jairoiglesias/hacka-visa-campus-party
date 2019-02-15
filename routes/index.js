3247ffe8afbeed7465c0f02dde7451ce9add3743
Jairo Iglesias <jairoiglesias@MacBook-Pro-de-Jairo.local>
2019-02-15 10:07:22 -0200

    Get Risk by Score

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ var router = express.Router();
 var soap = require('soap');
 var generator = require('creditcard-generator')
 
-var apiWSDL = 'https://ics2wstesta.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.153.wsdl'
-// var apiWSDL = 'https://ics2wsa.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.151.wsdl'
+let scoreData = [
+  { scoreIni: 0, scoreFim: 34, status: 'negado' },
+  { scoreIni: 35, scoreFim: 59, status: 'aprovado' },
+  { scoreIni: 60, scoreFim: 89, status: 'aprovado com 25% extra' },
+  { scoreIni: 90, scoreFim: 100, status: 'aprovado com 25% extra + 1%' }
+]
 
+var apiWSDL = 'https://ics2wstesta.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.151.wsdl'
+// var apiWSDL = 'https://ics2wsa.ic3.com/commerce/1.x/transactionProcessor/CyberSourceTransaction_1.151.wsdl'
 
 var args = {
   name: 'value'
@@ -22,8 +28,18 @@ soap.createClient(apiWSDL, function (err, client) {
     return
   }
 
-  console.log(client)
-  console.log('-----')
+  // console.log(client)
+  // console.log('-----')
+
+  var args = {
+    afsService_run: 'true',
+    merchantID: 'jiglesias',
+    afsService_run: 'true',
+    merchantReferenceCode: '2A0373972I4109O61207SX642',
+    billTo_street1: 'Avenida Doutor Augusto de Toledo, 1340',
+    billTo_contry: ' Brasil',
+    billTo_firstName: 'Emerson',
+  }
 
   // client.runTransactionAsync(args, (err, result) => {
 
@@ -45,8 +61,35 @@ router.get('/', function (req, res, next) {
 
 router.post('/get_score', (req, res) => {
 
-  let body = req.body
+  let { nome, cpf, cnpj, endereco, valor } = req.body
 
+})
+
+router.post('/analise_risco', (req, res) => {
+
+  // 35
+  // { scoreIni: 35, scoreFim: 59, status: 'aprovado' },
+
+  let { scoreUser } = req.body
+  // let scoreUser = 40
+
+
+  let score = scoreData.filter(scoreItem => {
+    let scoreIni = scoreItem.scoreIni
+    let scoreFim = scoreItem.scoreFim
+
+    console.log(scoreIni, scoreFim, scoreUser)
+
+    if (scoreUser >= scoreIni && scoreUser <= scoreFim) {
+      return scoreItem
+    }
+
+  })
+
+  let resp = JSON.stringify(score)
+  // console.log(resp)
+  // console.log('****')
+  res.status(200).send(resp)
 
 })
 
